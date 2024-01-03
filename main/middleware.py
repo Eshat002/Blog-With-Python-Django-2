7da52af95c32651556c2d4369a4000e7f8ee6e82
eshat002 <eshatjubayer22@outlook.com>
2024-01-03 20:15:16 +0600

    okay

--- main/middleware.py
+++ main/middleware.py
@@ -7,14 +7,14 @@ class CustomMiddleware:
 
     def __call__(self, request):
         # Get the client's IP address
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        print("x", x_forwarded_for)
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
-            print("ipssss",ip)
-        else:
-            ip = request.META.get('REMOTE_ADDR')
-            print("ipxxx", ip)
+        # x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        # print("x", x_forwarded_for)
+        # if x_forwarded_for:
+        #     ip = x_forwarded_for.split(',')[0]
+        #     print("ipssss",ip)
+        # else:
+        ip = request.META.get('REMOTE_ADDR')
+        print("ipxxx", ip)
         # Set the IP address in the request object
         request.client_ip = ip
 
